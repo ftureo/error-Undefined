74bc1621140193cc2cd06be9c516b4a790c6fa94
Fabián Tureo <ftureo@lanacion.com.ar>
2024-07-11 13:35:43 -0300

    fix: 🐛 invoke parser before router to able parse request

--- index.js
+++ index.js
@@ -3,21 +3,15 @@ const dbConection = require('./database/config');
 require('dotenv').config();
 const app = express();
 
-
 // Middleware
+// Cuando salga Undefined tenemos que hacer la lectura y parseo del body
+app.use(express.json());
 // Esto vincula una propiedad de express
-app.use('/auth' , require('./router/authRouter'));
+app.use('/auth', require('./router/authRouter'));
 
 // LLamamos a la funcion de conexion de base de datos
-
 dbConection();
 
-
-// Cuando salga Undefined tenemos que hacer la lectura y parseo del body
-
-app.use(express.json());
-
-
 app.listen(process.env.PORT, () => {
-   console.log(`Ejecutandose en el puerto ${process.env.PORT}`);
+    console.log(`Ejecutandose en el puerto ${process.env.PORT}`);
 });
