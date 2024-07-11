cb491a131b102e676a916a3a7ab3581b1981ea80
Fabián Tureo <ftureo@lanacion.com.ar>
2024-07-11 13:37:00 -0300

    style: 💄 improve code style and showing body property

--- router/authRouter.js
+++ router/authRouter.js
@@ -3,25 +3,19 @@ const express = require('express');
 // Esto nos va a permitir manejar todas las propiedades de express en este archivo
 const routherAuth = express.Router();
 
-
 routherAuth.get('/login', (req, res) => {
- 
-        res.send('Login');
+    res.send('Login');
     //  Aca se compara la lista de usuarios con la base de datos
     // Se hace un if que valide si ese usario existe o no en la bd y envia un msj 
 
 });
 
-
-routherAuth.post('/registro' , (req , res) => {
+routherAuth.post('/registro', (req, res) => {
     //La req se encarga de recibir los datos
-      console.log(req);
-      res.send('Registro');
+    console.log(req.body);
+    res.send('Registro');
 });
 
-
-
 // Exportamos 
-
 module.exports = routherAuth;
 
